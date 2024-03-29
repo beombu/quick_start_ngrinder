2d579033c133ddde5acfd897643d3f879ba90824
beombu <beombu13@gmail.com>
2024-03-30 04:47:08 +0900

    Feat : page조회 로직 캐시 구현

--- src/main/java/com/ngrinder/performance/controller/NoticeController.java
+++ src/main/java/com/ngrinder/performance/controller/NoticeController.java
@@ -2,9 +2,12 @@ package com.ngrinder.performance.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +28,11 @@ public class NoticeController {
 		List<Notice> notices = noticeService.getAllNotices();
 		return new ResponseEntity<>(notices, HttpStatus.OK);
 	}
+
+	@GetMapping("/{page}")
+	public ResponseEntity<Object> findByPage(HttpServletRequest request, @PathVariable("page") Integer page) {
+		List<Notice> notices = noticeService.findByPage(request, page);
+		return new ResponseEntity<>(notices, HttpStatus.OK);
+	}
+
 }
--- src/main/java/com/ngrinder/performance/service/NoticeServiceImpl.java
+++ src/main/java/com/ngrinder/performance/service/NoticeServiceImpl.java
@@ -31,8 +31,11 @@ public class NoticeServiceImpl implements NoticeService{
 	}
 
 	@Override
+	@Cacheable(value = "NoticeReadMapper.findByPage", key = "#request.requestURI + '-' + #pageNumber", condition = "#pageNumber <= 5")
 	public List<Notice> findByPage(HttpServletRequest request, int pageNumber) {
-		return null;
+		int startIdx = (pageNumber - 1) * 10;
+
+		return noticeReadMapper.findByPage(startIdx);
 	}
 
 	@Override
